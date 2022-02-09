44038a60aef3704e53e1a85a893141df84ce1f68
Caner Ayranci <cnrayrnci@gmail.com>
2022-02-09 19:54:33 +0300

    Add files via upload

---
+++ AlgorithmFinalQnA/Question5.java
@@ -0,0 +1,16 @@
+package AlgorithmFinalQnA;
+
+public class Question5 {
+    
+    // f=2 sigma i=1 3 pi j=1 4 sigma k=1 (i+j+k) write a java program
+    // which solves this three notation function.
+
+    public static void main(String[] args) {
+        
+        // I've no idea about how to solve this...
+        // I never seen three notations together so if anybody solves this
+        // please get in contact with me.
+
+    }
+
+}
